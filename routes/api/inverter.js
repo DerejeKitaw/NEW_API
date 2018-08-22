c35703328c97f3c66d5f386e29377d7c84f1fa0f
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-08-22 12:14:24 -0400

    inverter rout init

--- routes/api/inverter.js
+++ routes/api/inverter.js
@@ -1,9 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
+// Load Validation
+const validateInverterInput = require('../../validation/inverter');
+
+// Load inverter Model
+const inverter = require('../../models/inverter');
+// Load User Model
+const User = require('../../models/User');
+
 
 // @route  GET api/inverter/test
 // @desc   Tests inverter route
 // @access Public
 router.get('/test', (req, res) =>res.json({ msg: 'inverter Works'}));
 
+
+// @route  GET api/inverter/all
+// @desc   Get all inverters route
+// @access public
+router.get('/all', (req, res) =>res.json({ msg: 'inverter /all Works'}));
+
+
+// @route  POST api/inverter/create
+// @desc   Create inverter route
+// @access private
+router.post('/create', (req, res) =>res.json({ msg: 'inverter /create Works'}));
+
+
+// @route  POST api/inverter/update:id
+// @desc   Update inverter route
+// @access private
+router.post('/update/:id', (req, res) =>res.json({ msg: 'inverter /update/:id Works'}));
+
+
+
+// @route  POST api/inverter/delete:id
+// @desc   Delete inverter route
+// @access private
+router.post('/delete/:id', (req, res) =>res.json({ msg: 'inverter /delete/:id Works'}));
+
+
+// @route  POST api/inverter/edit/:id
+// @desc   Edit inverter route
+// @access private
+router.post('/edit/:id', (req, res) =>res.json({ msg: 'inverter edit/:id Works'}));
+
+
+
+
 module.exports = router;
+
