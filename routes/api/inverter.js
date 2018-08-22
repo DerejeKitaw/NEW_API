e0954170300b48cadd9263ab26851a75c264a8d1
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-08-22 13:07:25 -0400

    authenticate routes

--- routes/api/inverter.js
+++ routes/api/inverter.js
@@ -26,26 +26,30 @@ router.get('/all', (req, res) =>res.json({ msg: 'inverter /all Works'}));
 // @route  POST api/inverter/create
 // @desc   Create inverter route
 // @access private
-router.post('/create', (req, res) =>res.json({ msg: 'inverter /create Works'}));
+router.post('/create' , 
+  passport.authenticate('jwt', { session: false }), (req, res) =>res.json({ msg: 'inverter /create Works'}));
 
 
 // @route  POST api/inverter/update:id
 // @desc   Update inverter route
 // @access private
-router.post('/update/:id', (req, res) =>res.json({ msg: 'inverter /update/:id Works'}));
+router.post('/update/:id' ,
+  passport.authenticate('jwt', { session: false }) , (req, res) =>res.json({ msg: 'inverter /update/:id Works'}));
 
 
 
 // @route  POST api/inverter/delete:id
 // @desc   Delete inverter route
 // @access private
-router.post('/delete/:id', (req, res) =>res.json({ msg: 'inverter /delete/:id Works'}));
+router.post('/delete/:id' ,
+  passport.authenticate('jwt', { session: false }),  (req, res) =>res.json({ msg: 'inverter /delete/:id Works'}));
 
 
 // @route  POST api/inverter/edit/:id
 // @desc   Edit inverter route
 // @access private
-router.post('/edit/:id', (req, res) =>res.json({ msg: 'inverter edit/:id Works'}));
+router.post('/edit/:id' ,
+  passport.authenticate('jwt', { session: false }) , (req, res) =>res.json({ msg: 'inverter edit/:id Works'}));
 
 
 
