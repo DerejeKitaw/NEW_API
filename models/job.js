acda3cfb213f05e729b8d0a8f46338f19342638e
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-08-22 10:50:08 -0400

    comment added in model

--- models/job.js
+++ models/job.js
@@ -101,6 +101,28 @@ const JobSchema = new Schema({
       type: String
     }
   },
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
   date: {
     type: Date,
     default: Date.now
