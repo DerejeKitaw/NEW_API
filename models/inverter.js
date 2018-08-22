aca9d7dcf7f232f0bac29c3d2c9c1de0888da4a1
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-08-22 09:17:31 -0400

    Create Inverter model

---
+++ models/inverter.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const InverterSchema = new Schema({
+  inverterManufacturer: {
+    type: String,
+    required: true
+  },
+  inverterType: {
+    type: String,
+    required: true
+  },
+  maxPowerOutput: {
+    type: String,
+    required: true
+  },
+  acOutputVoltage: {
+    type: String,
+    required: true
+  },
+  maxAcCurrentOutpu: {
+    type: String,
+    required: true
+  },
+  maxDcVoltage: {
+    type: String,
+    required: true
+  },
+  startupVoltage: {
+    type: String,
+    required: true
+  },
+  cec: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Inverter = mongoose.model('inverterss', InverterSchema);
